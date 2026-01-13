c99804e39a1daae2a081477900644a4c0cf1cac1
XavierFarrus <xavier.farrus@gracia.lasalle.cat>
2026-01-13 18:26:43 +0100

    Proyecto completo

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,4 +57,8 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+    implementation("androidx.navigation:navigation-compose:2.7.5")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.7")
+    implementation("androidx.compose.runtime:runtime-livedata")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.8.7")
 }
--- app/src/main/java/com/example/lazycomponentsapp/MainActivity.kt
+++ app/src/main/java/com/example/lazycomponentsapp/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.lazycomponentsapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -11,37 +10,54 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.lazycomponentsapp.navigation.Routes
 import com.example.lazycomponentsapp.ui.theme.LazyComponentsAppTheme
+import com.example.lazycomponentsapp.view.CardDetailScreen
+import com.example.lazycomponentsapp.view.CardListScreen
+import com.example.lazycomponentsapp.viewmodel.CardListViewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
         setContent {
             LazyComponentsAppTheme {
+                val navController = rememberNavController()
+                val cardVm: CardListViewModel = viewModel()
+
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    NavHost(
+                        navController = navController,
+                        startDestination = Routes.ListScreen.route,
                         modifier = Modifier.padding(innerPadding)
-                    )
+                    ) {
+
+                        composable(Routes.ListScreen.route) {
+                            CardListScreen(navController = navController, vm = cardVm)
+                        }
+
+                        composable(
+                            route = Routes.DetailScreen.route,
+                            arguments = listOf(navArgument("cardIndex") { type = NavType.StringType })
+                        ) { backStackEntry ->
+                            val cardIndex = backStackEntry.arguments?.getString("cardIndex").orEmpty()
+                            CardDetailScreen(navController = navController, cardIndex = cardIndex, vm = cardVm)
+                        }
+                    }
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    LazyComponentsAppTheme {
-        Greeting("Android")
-    }
+fun Preview() {
+    LazyComponentsAppTheme { Text("Preview") }
 }
